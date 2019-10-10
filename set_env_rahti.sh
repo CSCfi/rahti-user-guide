1144855b100b7578c30e4dfed974bac30856b39e
juhani.kataja <juhani.kataja@csc.fi>
2019-10-10 15:57:39 +0300

    make documentation depend heavily on docs.csc.fi

--- set_env_rahti.sh
+++ set_env_rahti.sh
@@ -2,10 +2,11 @@
 
 export BILLING_ENABLED=False
 export GITLAB_LOGIN_SUPPORT=False
-export LDAP_LOGIN_SUPPORT=True
+export LDAP_LOGIN_SUPPORT=False
 export OPENSHIFT_VERSION=3.11
 export OSO_REGISTRY_URL=https://registry-console.rahti.csc.fi
 export OSO_WEB_UI_URL=https://rahti.csc.fi:8443
-export SUI_INTEGRATION_DONE=False
+export SUI_INTEGRATION_DONE=True
 export SYSTEM_NAME=Rahti
 export SHOW_AGREEMENTS=True
+export DOCS_PREFIX=http://localhost:8001/#cloud/rahti
