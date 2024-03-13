535e517f0f22122c26f65aecfd2272ee6838c0cf
alvaro.gonzalez <alvaro.gonzalez@csc.fi>
2024-03-13 16:11:18 +0200

    First drat, copied form wiki. Only a mockup

--- make_config.sh
+++ make_config.sh
@@ -5,4 +5,7 @@ set -e
 : ${CLUSTER_LOGIN_URL_OIDCIDP?"Must set CLUSTER_LOGIN_URL_OIDCIDP"}
 : ${CLUSTER_LANDING_PAGE_SECONDARY_ENV_VERSION?"Must set CLUSTER_LANDING_PAGE_SECONDARY_ENV_VERSION"}
 
+mkdir -p /usr/share/nginx/html/agreements/terms_of_use/
+
 yasha -o /usr/share/nginx/html/index.html index.html.j2
+yasha -o /usr/share/nginx/html/agreements/terms_of_use/index.html terms_of_use.html.j2
