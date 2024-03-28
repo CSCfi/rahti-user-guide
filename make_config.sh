57d2a80852b34d9ac04fbbe05e3cf2734c62e620
Ahsan Feroz <ahsan.feroz@csc.fi>
2024-03-28 12:32:48 +0200

    fix href reference for the directory structure

--- make_config.sh
+++ make_config.sh
@@ -5,9 +5,9 @@ set -e
 : ${CLUSTER_LOGIN_URL_OIDCIDP?"Must set CLUSTER_LOGIN_URL_OIDCIDP"}
 : ${CLUSTER_LANDING_PAGE_SECONDARY_ENV_VERSION?"Must set CLUSTER_LANDING_PAGE_SECONDARY_ENV_VERSION"}
 
-mkdir -p /usr/share/nginx/html/agreements/terms_of_use/
-mkdir -p /usr/share/nginx/html/accessibility/
+# mkdir -p /usr/share/nginx/html/agreements/terms_of_use/
+# mkdir -p /usr/share/nginx/html/accessibility/
 
 yasha -o /usr/share/nginx/html/index.html index.html.j2
-yasha -o /usr/share/nginx/html/agreements/terms_of_use/terms_of_use.html terms_of_use.html.j2
-yasha -o /usr/share/nginx/html/accessibility/accessibility_statement.html accessibility_statement.html.j2
+yasha -o /usr/share/nginx/html/terms_of_use.html terms_of_use.html.j2
+yasha -o /usr/share/nginx/html/accessibility_statement.html accessibility_statement.html.j2
