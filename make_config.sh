f4ad4532ca9fd8940101e0d47e5e4de08f4cad91
alvaro.gonzalez <alvaro.gonzalez@csc.fi>
2024-05-24 14:00:23 +0300

    Add old SLA text

--- make_config.sh
+++ make_config.sh
@@ -6,9 +6,11 @@ set -e
 : ${CLUSTER_LANDING_PAGE_SECONDARY_ENV_VERSION?"Must set CLUSTER_LANDING_PAGE_SECONDARY_ENV_VERSION"}
 
 mkdir -p /usr/share/nginx/html/agreements/terms_of_use/
+mkdir -p /usr/share/nginx/html/agreements/sla/
 # mkdir -p /usr/share/nginx/html/accessibility/
 
 yasha -o /usr/share/nginx/html/index.html index.html.j2
 yasha -o /usr/share/nginx/html/terms_of_use.html terms_of_use.html.j2
 yasha -o /usr/share/nginx/html/agreements/terms_of_use/index.html terms_of_use.html.j2
+yasha -o /usr/share/nginx/html/agreements/sla/index.html sla.html.j2
 yasha -o /usr/share/nginx/html/accessibility_statement.html accessibility_statement.html.j2
